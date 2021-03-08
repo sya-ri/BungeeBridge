04b335cded62a4da936194eaf391c722391da935
sya-ri <sya79lua@gmail.com>
2021-03-09 01:34:06 +0900

    server: 更新がなかった場合に自動でプレイヤーを削除するようにした

--- server/src/main/kotlin/com/github/syari/bungeebridge/server/PlayerContainer.kt
+++ server/src/main/kotlin/com/github/syari/bungeebridge/server/PlayerContainer.kt
@@ -1,14 +1,27 @@
 package com.github.syari.bungeebridge.server
 
+import com.github.syari.bungeebridge.server.properties.Option
+import java.util.Timer
+import java.util.TimerTask
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.concurrent.timerTask
 
 object PlayerContainer {
     private val servers = ConcurrentHashMap<String, MutableSet<String>>()
+    private val crashWatchdogTasks = ConcurrentHashMap<String, TimerTask>()
 
     fun update(name: String, join: Set<String>, quit: Set<String>) {
         servers.getOrPut(name, ::mutableSetOf).run {
             removeAll(quit)
             addAll(join)
+        }
+        crashWatchdogTasks[name]?.cancel()
+        crashWatchdogTasks[name] = timerTask {
+            clear(name)
+            crashWatchdogTasks.remove(name)
+            BungeeBridge.logger.info("$name is Not Updated. Delete All Players.")
+        }.apply {
+            Timer().schedule(this, Option.CrashTime)
         }
     }
 
--- server/src/main/kotlin/com/github/syari/bungeebridge/server/properties/Option.kt
+++ server/src/main/kotlin/com/github/syari/bungeebridge/server/properties/Option.kt
@@ -2,6 +2,8 @@ package com.github.syari.bungeebridge.server.properties
 
 object Option : PropertiesFile("option.properties") {
     private val PortAsString by keyValue("Port")
+    private val CrashTimeString by keyValue("CrashTime")
 
     val Port = PortAsString.toIntOrNull() ?: 8080
+    val CrashTime = CrashTimeString.toLongOrNull() ?: 30000L
 }
